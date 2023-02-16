4bae21c61b1ea4f25cdd3e12826848807ab8534a
Jhosef Reyes <jhosefgrey@gmail.com>
2023-02-16 17:00:31 -0600

    base

--- src/routes/order.js
+++ src/routes/order.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const orderSchema = require("../models/order");
+const userSchema = require("../models/user");
 const productSchema = require("../models/products");
 
+const service = require("../services/whatsapp.service");
+const modelWp = require("../shared/wp.model");
 
 //create order
 router.post('/order', async (req, res) => {
@@ -13,20 +16,62 @@ router.post('/order', async (req, res) => {
         
         const order = orderSchema(req.body);
 
+        let prods =[];
+
+        let user = await userSchema.findOne({ user_id: order.user_id});
+
         for (let i = 0; i < order.products.length; i++) {
             const prod = order.products[i];
-            let objPrice =  await productSchema.findOne({ _id :prod}).select('price');            
+            let objPrice =  await productSchema.findOne({ _id :prod});            
             order.total =  order.total + objPrice.price;
+
+
+            let pr = await prods.find((x)=> x.name === objPrice.name);
+
+            if(!pr){
+                prods.push({
+                    quantity: 1,
+                    name: objPrice.name,
+                })
+                console.log('nuevo');
+            }else{
+                pr.quantity = pr.quantity + 1;
+            }
+
         }
+
+        console.log('prods',prods);
+
+        var prsOrder = "";
+        
+        for (let i = 0; i < prods.length; i++) {
+            const pr = prods[i];
+            
+            prsOrder = prsOrder.concat(`\n`, `*${pr.quantity} - ${pr.name}*`)
+
+        }
+
+        console.log('prsOrder', prsOrder);
      
         order.save()
         .then(result => {
+
+    
+            service.SendMessageWh(
+                modelWp.MessageText(
+                    `Perfecto ${user.name} tu pedido es:\n${prsOrder}\nCon un total de Q.${order.total}`,
+                    order.user_id
+                )
+            );
+
             res.status(200).json({
                 message: "orden creada",
                 obj: result._id
             });
+
         })
         .catch(err => {
+            console.log('err save',err)
             res.status(500).json({
                 error: err
             });
